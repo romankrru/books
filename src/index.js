0d8abadd6f00d5457e389df685d22de385ad50d7
Roman <rm07ru@gmail.com>
2018-02-15 19:39:51 +0300

    Add redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, compose } from 'redux';
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './App';
+import rootReducer from './store/reducers/books';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let composeEnhancers;
+
+if (process.env.NODE_ENV === 'development') {
+  composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+} else {
+  composeEnhancers = compose;
+}
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(),
+);
+
+const app = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();
---
+++ src/store/reducers/books.js
@@ -0,0 +1,13 @@
+const initialState = {
+  isEdititng: false,
+  booksList: [],
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducer;
