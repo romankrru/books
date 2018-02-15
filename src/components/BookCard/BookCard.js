fee59d6713e823606019653d7368966ff4c510ab
Roman <rm07ru@gmail.com>
2018-02-15 19:17:44 +0300

    Add BookCard

---
+++ src/components/BookCard/BookCard.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import styles from './BookCard.css';
+import Button from '../Button/Button';
+
+const BookCard = props => {
+  const { data } = props;
+
+  return (
+    <li className={styles.BookCard}>
+      <div className={styles.BookCardRow}>
+        <h4 className={styles.BookCardHeading}>Author:</h4>{' '}
+        <p className={styles.BookCardText}>{data.author}</p>
+      </div>
+      <div className={styles.BookCardRow}>
+        <h4 className={styles.BookCardHeading}>Book name:</h4>{' '}
+        <p className={styles.BookCardText}>{data.name}</p>
+      </div>
+      <div className={styles.BookCardRow}>
+        <h4 className={styles.BookCardHeading}>Year:</h4>{' '}
+        <p className={styles.BookCardText}>{data.year}</p>
+      </div>
+      <div className={styles.BookCardRow}>
+        <h4 className={styles.BookCardHeading}>Pages:</h4>{' '}
+        <p className={styles.BookCardText}>{data.pages}</p>
+      </div>
+      <div>
+        <Button>Edit</Button>
+        <Button>Delete</Button> 
+      </div>
+    </li>
+  );
+};
+
+export default BookCard;
